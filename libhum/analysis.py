Fixes overflow error in Numpy arrays.

libhum/analysis.py
@@ -362,6 +362,7 @@ def _post_process_enf(enf: np.ndarray, snrs: np.ndarray) -> np.ma.masked_array:
         (smoothed < -SPECTRUM_BAND_SIZE) | (smoothed > SPECTRUM_BAND_SIZE),
         smoothed
     )
+    clipped.fill_value = np.nan
 
     thresholded = _threshold_enf(clipped, snrs)
 
@@ -495,7 +496,7 @@ def _threshold_enf(enf: np.ma.masked_array, snrs: np.ndarray) -> np.ma.masked_ar
 
         i += 1
 
-    return np.ma.array(enf, mask=enf.mask | thres_mask)
+    return np.ma.array(enf, mask=enf.mask | thres_mask, fill_value=np.nan)
 
 
 def _combined_harmonics_result(

libhum/types.py
@@ -181,9 +181,7 @@ class AnalysisResult:
 
         f, t, Zxx = self.spectrum
 
-        spectrum_harmonic = self.frequency_harmonic
-
-        ax.pcolormesh(t, f / spectrum_harmonic, Zxx, shading='gouraud')
+        ax.pcolormesh(t, f / self.frequency_harmonic, Zxx, shading='gouraud')
         ax.plot(
             t,
             self.enf.signal.astype(np.float64) + self.enf.network_frequency,